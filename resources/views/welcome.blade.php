f0b56049002efe245afd62e6f2910debb003635b
Ruhin Hossain <h.ruhin@yahoo.com>
2020-03-10 22:43:37 +0600

    this is another commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Laravel
+                    Welcome to Laravel 
                 </div>
 
                 <div class="links">
